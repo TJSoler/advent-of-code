6c5e5c42ed9946639d0b00cbb79c8ca4f3e50317
Tomás Soler <tomas@tjsoler.me>
2020-12-05 18:57:32 +0100

    Add unit tests and cleanup of day 3

--- go/2020/03/main.go
+++ go/2020/03/main.go
@@ -7,6 +7,8 @@ import (
 	"github.com/tjsoler/advent-of-code/go/2020/helpers"
 )
 
+var lines = helpers.ReadFileToStringSlice("./2020/03/input.txt")
+
 func Register() {
 	fmt.Println("Day 03")
 	a()
@@ -15,25 +17,25 @@ func Register() {
 
 func a() {
 	fmt.Printf("\tPart A")
-	fmt.Printf(", solution: %d\n", checkForTrees(3, 1))
+	fmt.Printf(", solution: %d\n", checkForTrees(lines, 3, 1))
 }
 
 func b() {
 	fmt.Printf("\tPart B")
 
-	slope1 := checkForTrees(1, 1)
-	slope2 := checkForTrees(3, 1)
-	slope3 := checkForTrees(5, 1)
-	slope4 := checkForTrees(7, 1)
-	slope5 := checkForTrees(1, 2)
+	slope1 := checkForTrees(lines, 1, 1)
+	slope2 := checkForTrees(lines, 3, 1)
+	slope3 := checkForTrees(lines, 5, 1)
+	slope4 := checkForTrees(lines, 7, 1)
+	slope5 := checkForTrees(lines, 1, 2)
 
 	product := slope1 * slope2 * slope3 * slope4 * slope5
 
 	fmt.Printf(", solution: %d\n", product)
 }
 
-func checkForTrees(moveX int, moveY int) (treeCount int) {
-	lines := helpers.ReadFileToStringSlice("./2020/03/input.txt")
+func checkForTrees(lines []string, moveX int, moveY int) (treeCount int) {
+
 	x, y := 0, 0
 	tree := '#'
 
